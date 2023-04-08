713352b192612d0f41623c8a5e509c01edcdc922
anigvo <anigvo@gmail.com>
2023-04-08 14:43:48 +0300

    update lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`);
+const galleryMarkup = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML(`beforeend`, galleryMarkup);
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `        
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
+    })
+    .join(``);
+}
+
+const lightboxGallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: `img`,
+  captionType: `attr`,
+  captionPosition: `bottom`,
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
+gallery.addEventListener(`click`, onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+}
+
